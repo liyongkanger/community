1110efaf997224026f3652fbd7ed554cbfc29aa0
liyongkanger <745519261@qq.com>
2020-03-01 15:46:38 +0800

    extract github config code to properties

---
+++ src/main/java/lyk/community/provider/GithubProvider.java
@@ -0,0 +1,42 @@
+package lyk.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import lyk.community.dto.AccessTokenDTO;
+import lyk.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+   public String getAccessToken(AccessTokenDTO accessTokenDTO) throws IOException {
+       final MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+       OkHttpClient client = new OkHttpClient();
+
+               RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+           Request request = new Request.Builder()
+                   .url("https://github.com/login/oauth/access_token")
+                   .post(body)
+                   .build();
+           try (Response response = client.newCall(request).execute()) {
+               String string = response.body().string();
+               String token = string.split("&")[0].split("=")[1];
+               return token;
+
+           }
+   }
+
+   public GithubUser getUser(String accessToken) throws IOException {
+       OkHttpClient client = new OkHttpClient();
+       Request request = new Request.Builder()
+               .url("https://api.github.com/user?access_token="+accessToken)
+               .build();
+       Response response = client.newCall(request).execute();
+       assert response.body() != null;
+       String string = response.body().string();
+       GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+       return githubUser;
+    }
+}
